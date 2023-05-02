5842363245b84ee6d31777e272c130587d592111
Joana2712 <a19428@alunos.ipca.pt>
2023-05-02 19:18:46 +0100

    Cutscene Inicial

---
+++ PI_2_GAME/Assets/StartGameafterCS.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Playables;
+
+
+public class StartGameafterCS : MonoBehaviour
+{
+    [SerializeField] private GameObject fpcamera;
+    [SerializeField] public PlayerController player;
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private PlayableDirector director;
+    [SerializeField] private GameObject cutscene;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.OnDisable();
+        
+       fpcamera.SetActive(false);
+       _characterController.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnEndCutScene();
+    }
+
+    void OnEndCutScene()
+    {
+        if (director.state != PlayState.Playing)
+        {
+            cutscene.SetActive(false);
+            fpcamera.SetActive(true);
+            player.OnEnable();
+            _characterController.enabled = true;
+        }
+        
+    }
+}
