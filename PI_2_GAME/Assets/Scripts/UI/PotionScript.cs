525b787fdd719959ff340a859a042d618c2f7abc
Nunox73 <32603408+Nunox73@users.noreply.github.com>
2023-05-07 16:27:55 +0100

    Melhorar a barra do cooldown - 1st version

--- PI_2_GAME/Assets/Scripts/UI/PotionScript.cs
+++ PI_2_GAME/Assets/Scripts/UI/PotionScript.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 
 public class PotionScript : MonoBehaviour
 {
-
+    public Image oldImage;
     public Sprite potion_100, potion_80, potion_60, potion_40, potion_20, potion_0;
     Image potionImage;
    
 
     private void Awake()
     {
-        potionImage = GetComponent<Image>();
+       
     }
     // Start is called before the first frame update
     void Start()
@@ -31,8 +31,28 @@ public class PotionScript : MonoBehaviour
 
         if (valor < 100)
         {
-            Debug.Log(valor);
+            if (valor < 20) {
+                oldImage.sprite = potion_0;
+            } else if (valor < 40)
+            {
+                oldImage.sprite = potion_20;
+            } else if (valor < 60)
+            {
+                oldImage.sprite = potion_40;
+            } else if (valor < 80)
+            {
+                oldImage.sprite = potion_60;
+            } else if (valor < 100)
+            {
+                oldImage.sprite = potion_80;
+            } else if (valor == 100)
+            {
+                oldImage.sprite = potion_100;
+            }
+
+
         }
-        //potionImage.sprite = potion_40;
+        
+
     }
 }
