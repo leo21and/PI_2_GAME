9a7eee5f3b50f6e65a041760807516d2a10609c5
Nunox73 <32603408+Nunox73@users.noreply.github.com>
2023-05-18 17:59:01 +0100

    Player Electric Beams

--- PI_2_GAME/Assets/Scripts/BlackWizard/ThunderPlayerCollision.cs
+++ PI_2_GAME/Assets/Scripts/BlackWizard/ThunderPlayerCollision.cs
@@ -6,12 +6,20 @@ public class ThunderPlayerCollision : MonoBehaviour
 {
     public PlayerDamage playerDamage;
     public int thunderDamage = 50;
+    public GameObject Electric_Beams;
 
     private void OnParticleCollision(GameObject other)
     {
 
+        Electric_Beams.SetActive(true);
         Debug.Log("Thunder Collider");
         playerDamage.currentHealth = playerDamage.currentHealth - thunderDamage;
+        Invoke("Disable_Electric_Beams", 1);
+        
+    }
 
+    void Disable_Electric_Beams()
+    {
+        Electric_Beams.SetActive(false);
     }
 }
