5b79f110cd148919919a9b7627788cb97c609a7f
Nunox73 <32603408+Nunox73@users.noreply.github.com>
2023-05-22 20:16:57 +0100

    Feiticeiro Negro - substituir as plantas pelas finais (1 a 4 das Flowers)

--- PI_2_GAME/Assets/Scripts/BlackWizard/LookAtObject.cs
+++ PI_2_GAME/Assets/Scripts/BlackWizard/LookAtObject.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class LookAtObject : MonoBehaviour
 {
     [SerializeField] Transform player;
+
+    void Start()
+    {
+        if (GameObject.FindWithTag("Player") != null)
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+    }
+
     void Update()
     {
         transform.LookAt(player);
+
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+
+
     }
     
 
